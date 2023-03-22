f5a5ec08dad46e899409396217a4f0e4ca90f2a2
Vishal Prajapati <vp22052001@gmail.com>
2023-03-22 20:35:36 +0530

    Parent Page for two-way binding

---
+++ src/views/ParentCompo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <form @submit.prevent="handleSubmit">
+            <TextInput label="email" :model-values="email" /> {{ email }}
+            <TextInput label="password" :model-values="password" />
+            <button>Submit</button>
+        </form>
+    
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+import TextInput from '../components/TextInput.vue'
+import { ref, } from 'vue';
+
+
+
+const email = ref("data")
+const password = ref("")
+
+function handleSubmit() {
+    console.log("called");
+    console.log("Email = ",email.value);
+    console.log("Password = ",password.value);
+}
+
+</script>
+
+
+
+<style>
+
+
+</style>
